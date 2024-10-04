46761fd2fa620b810237b22c053e2b37ccf817a7
Jesus Madrid <114009973+JesusMadridMCH@users.noreply.github.com>
2024-10-04 17:20:29 -0500

    task_01-Inventory-Flow-Service:  Creating inventoryFlowService. Create registerProductFlow to keep tracked when a transaction should be marked as iN or OUT for the inventory

---
+++ inventory-app/src/main/java/com/inventory/service/InventoryFlowService.java
@@ -0,0 +1,35 @@
+package com.inventory.service;
+
+import com.inventory.mapper.InventoryFlowMapper;
+import com.inventory.repository.InventoryFlowRepository;
+import com.inventory.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import com.inventory.model.InventoryFlowServiceException;
+import com.inventory.model.ProductFlow;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+public class InventoryFlowService {
+    private final InventoryFlowRepository inventoryFlowRepository;
+    private final ProductRepository productRepository;
+    private final InventoryFlowMapper inventoryFlowMapper;
+
+    public Mono<Long> registerProductFlow(ProductFlow productFlow) {
+        return
+            Mono.just(productFlow)
+                .flatMap(productFlowForRegistering -> {
+                    try {
+                        return
+                            productRepository.findByCode(productFlow.getCode())
+                                .map(productDb -> {
+                                    var inventoryFlow = inventoryFlowMapper.fromProductFlow(productFlow);
+                                    var newInventoryFlow = inventoryFlowRepository.save(inventoryFlow);
+                                    return Mono.just(newInventoryFlow.getId());
+                                }).orElseGet(() -> Mono.error(new InventoryFlowServiceException("There was an error trying to register the product flow")));
+                    } catch (Exception e) {
+                        return Mono.error(new InventoryFlowServiceException("There was an error trying to find the Code related to the Product Flow"));
+                    }
+                })
+                .switchIfEmpty(Mono.error(new InventoryFlowServiceException("Product Flow must not be NULL to be registered")));
+    }
+}
